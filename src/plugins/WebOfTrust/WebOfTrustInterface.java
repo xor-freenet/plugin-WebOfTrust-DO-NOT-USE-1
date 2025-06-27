a16d473804a0623b87efa042f3e9f5757074a72a
xor-freenet <xor@freenetproject.org>
2025-06-27 17:12:08 +0200

    Seed identities: Update edition numbers

--- src/plugins/WebOfTrust/WebOfTrustInterface.java
+++ src/plugins/WebOfTrust/WebOfTrustInterface.java
@@ -41,14 +41,14 @@ public abstract class WebOfTrustInterface {
 	 * the Freenet development team provides a list of seed identities. These are identities of both developers and long term users.
 	 */
 	static final String[] SEED_IDENTITIES = new String[] { 
-		"USK@QeTBVWTwBldfI-lrF~xf0nqFVDdQoSUghT~PvhyJ1NE,OjEywGD063La2H-IihD7iYtZm3rC0BP6UTvvwyF5Zh4,AQACAAE/WebOfTrust/1613", // xor
+		"USK@QeTBVWTwBldfI-lrF~xf0nqFVDdQoSUghT~PvhyJ1NE,OjEywGD063La2H-IihD7iYtZm3rC0BP6UTvvwyF5Zh4,AQACAAE/WebOfTrust/1615", // xor
 		// "USK@z9dv7wqsxIBCiFLW7VijMGXD9Gl-EXAqBAwzQ4aq26s,4Uvc~Fjw3i9toGeQuBkDARUV5mF7OTKoAhqOA9LpNdo,AQACAAE/WebOfTrust/5156", // Toad - no longer active
-		"USK@o2~q8EMoBkCNEgzLUL97hLPdddco9ix1oAnEa~VzZtg,X~vTpL2LSyKvwQoYBx~eleI2RF6QzYJpzuenfcKDKBM,AQACAAE/WebOfTrust/24077", // Bombe (non-personal seed-only identity)
+		"USK@o2~q8EMoBkCNEgzLUL97hLPdddco9ix1oAnEa~VzZtg,X~vTpL2LSyKvwQoYBx~eleI2RF6QzYJpzuenfcKDKBM,AQACAAE/WebOfTrust/24098", // Bombe (non-personal seed-only identity)
 		// "USK@D3MrAR-AVMqKJRjXnpKW2guW9z1mw5GZ9BB15mYVkVc,xgddjFHx2S~5U6PeFkwqO5V~1gZngFLoM-xaoMKSBI8,AQACAAE/WebOfTrust/8231", // zidel - no longer active
 		// "USK@nmTkFmn0Akz1-G9iIN2w6lsQEAfWkpQw3ckOxtwMh2Q,Du9GQxGDj0Nax4zN9-ANTPetx-GxOoWaRf6Gq6Nbh1o,AQACAAE/WebOfTrust/12707", // operhiem1 - no longer active
-		"USK@-wFZ6ezwU3QgvM1u8~uiLbtjxSQ19tuqn4Q94lvTrwE,7Cyi6MgbnwnQmXQcLdl8y~snFUtrEle3bghkqwNLoMw,AQACAAE/WebOfTrust/7055", // HieronymusCH
+		"USK@-wFZ6ezwU3QgvM1u8~uiLbtjxSQ19tuqn4Q94lvTrwE,7Cyi6MgbnwnQmXQcLdl8y~snFUtrEle3bghkqwNLoMw,AQACAAE/WebOfTrust/7070", // HieronymusCH
 		"USK@8AEGotJKXJ4ABJy1gKjls4SrrzpshQNoEMAbu0IFA94,blTuIOwf7k9p1R2GCfJSPSaOFzsuXLrFujOnV1Irjag,AQACAAE/WebOfTrust/11821", // Adilson_Lanpo
-		"USK@6~ZDYdvAgMoUfG6M5Kwi7SQqyS-gTcyFeaNN1Pf3FvY,OSOT4OEeg4xyYnwcGECZUX6~lnmYrZsz05Km7G7bvOQ,AQACAAE/WebOfTrust/16874", // ArneBab
+		"USK@6~ZDYdvAgMoUfG6M5Kwi7SQqyS-gTcyFeaNN1Pf3FvY,OSOT4OEeg4xyYnwcGECZUX6~lnmYrZsz05Km7G7bvOQ,AQACAAE/WebOfTrust/16903", // ArneBab
 	};
 	
 	abstract public List<Identity> getAllIdentities();
